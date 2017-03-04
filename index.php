60465d288cb7813afc67d3038c11e12d516d8c7c
maximumayo <maxgmayo@gmail.com>
2017-03-04 14:26:51 -0500

    skeleton login page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>simpleCMS</title>
+
+</head>
+<body>
+
+<h2>Login</h2>
+<form action="process_login.php" method="post"></form>
+
+<label for="username">Username</label>
+<input type="text" name="username" id="username">
+
+<label for="password">Password</label>
+<input type="password" name="password" id="password">
+
+<input type="submit" value="Login">
+
+</body>
+</html>
