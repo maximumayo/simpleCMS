8e8519076d95627af2b89c4bf2a9fca40e4bcf8e
maximumayo <maxgmayo@gmail.com>
2017-03-14 20:23:10 -0400

    prevent empty fields user update page

--- users/process_edit_user.php
+++ users/process_edit_user.php
@@ -4,6 +4,18 @@ require_once(__DIR__ . "/../includes/functions.php");
 
 $id = $_POST['id'];
 
+//fields must not be left blank
+$toBeValidated = ['username', 'first_name', 'last_name'];
+
+$validation = doValidation($_POST, $toBeValidated);
+
+if (!$validation[0]) {
+    $error = $validation[1];
+    $error = http_build_query(array('error' => $error));
+    header("Location: /../Projects/simpleCMS/users/edit_user.php?id={$id}&" . $error);
+    exit;
+}
+
 if (editUser($id, $_POST)) {
     $user = getUser($id);
     $_SESSION['username'] = $user["username"];
